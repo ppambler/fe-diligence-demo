32bb8541c07a67cf6f39088003a9b83ce9d0aabc
ppambler <allinlbj@gmail.com>
2022-01-06 00:21:25 +0800

    命令行交互，标记任务状态、更新/删除 旧任务、创建新任务

--- nodejs/01-file-system/node-todo/index.js
+++ nodejs/01-file-system/node-todo/index.js
@@ -1,4 +1,5 @@
 const db = require("./db.js");
+const inquirer = require("inquirer");
 module.exports.add = async (title) => {
   //读取之前的任务
   const list = await db.read();
@@ -21,7 +22,87 @@ module.exports.showAll = async () => {
   //读取之前的任务
   const list = await db.read();
   //打印之前的任务
-  list.forEach((task, index) => {
-    console.log(`${task.done ? "✔" : "🟨"} ${index + 1} - ${task.title}`);
-  });
+  // list.forEach((task, index) => {
+  //   console.log(`${task.done ? "[✔]" : "[]"} ${index + 1} - ${task.title}`);
+  // });
+  inquirer
+    .prompt({
+      type: "list",
+      name: "index",
+      message: "请选择你想要操作的任务",
+      choices: [
+        { name: "退出", value: "-1" },
+        ...list.map((task, index) => {
+          return {
+            name: `${task.done ? "[✔]" : "[_]"} ${index + 1} - ${task.title}`,
+            value: index.toString(),
+          };
+        }),
+        { name: "+ 创建任务", value: "-2" },
+      ],
+    })
+    .then((answer) => {
+      const current = parseInt(answer.index);
+      const index = current + 1;
+      if (index > 0) {
+        //选择了一个任务
+        inquirer
+          .prompt({
+            type: "list",
+            name: "action",
+            message: "请选择操作",
+            choices: [
+              { name: "退出", value: "quit" },
+              { name: "已完成", value: "markAsDone" },
+              { name: "未完成", value: "markAsUnDone" },
+              { name: "改标题", value: "updateTitle" },
+              { name: "删除", value: "remove" },
+            ],
+          })
+          .then((answer2) => {
+            switch (answer2.action) {
+              case "markAsDone":
+                list[current].done = true;
+                db.write(list);
+                break;
+              case "markAsUnDone":
+                list[current].done = false;
+                db.write(list);
+                break;
+              case "updateTitle":
+                inquirer
+                  .prompt({
+                    type: "input",
+                    name: "title",
+                    message: "新的标题",
+                    default: list[current].title,
+                  })
+                  .then((answer) => {
+                    list[current].title = answer.title;
+                    db.write(list);
+                  });
+                break;
+              case "remove":
+                list.splice(current, 1);
+                db.write(list);
+                break;
+            }
+          });
+      } else if (index === -1) {
+        //创建一个任务
+        inquirer
+          .prompt({
+            type: "input",
+            name: "title",
+            message: "输入任务标题",
+          })
+          .then((answer) => {
+            list.push({
+              title: answer.title,
+              done: false,
+            });
+            db.write(list);
+          });
+      }
+    });
 };
