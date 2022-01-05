c3d4747285e0bef28407e33e80d1c84847bca54e
ppambler <allinlbj@gmail.com>
2022-01-05 20:23:40 +0800

    测试 command.js

--- nodejs/01-file-system/node-todo/index.js
+++ nodejs/01-file-system/node-todo/index.js
@@ -2,7 +2,10 @@ const { Command } = require("commander");
 const program = new Command();
 
 // 选项用 - 或 -- 开头
-program.option("-x, --xxx", "this is xxx").option("-y, --yyy", "this is yyy");
+program
+  .version("0.0.1")
+  .option("-x, --xxx", "this is xxx")
+  .option("-y, --yyy", "this is yyy");
 
 // 这个字符串里边的 <> 是有意义的 -> node index.js 1 2 -> action 会 log 出 1 2
 // action 即 这个子命令的作用是啥
@@ -30,15 +33,12 @@ program
   .action((args) => {
     const words = args.join(" ");
     console.log(words);
+    const options = program.opts();
+    console.log(options);
   });
-
-console.log(process.argv);
 
 // option 和 command 的注册必须放在 parse 之前
 program.parse(process.argv);
 
 // options -> 你用了 -x 或 -xxx，那么它的值就是 { xxx: true }
 // 我同时写 option 和 command，option 即失效了，不管我输入 -x -y 还是什么的，都会展示 -h 的效果
-const options = program.opts();
-if (options.xxx) console.log(options);
-if (options.yyy) console.log("- this is yyy");
