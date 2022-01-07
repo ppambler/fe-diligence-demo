b3913b302eb8818cfad256e3ca0e1aada0f1b775
ppambler <allinlbj@gmail.com>
2022-01-07 23:58:59 +0800

    给 Buffer.concat 添加一个总长度

--- nodejs/02-http/node-static-server/index.ts
+++ nodejs/02-http/node-static-server/index.ts
@@ -8,13 +8,16 @@ server.on("request", (request: IncomingMessage, response: ServerResponse) => {
   console.log(request.url);
   console.log(request.headers);
   const array = [];
+  let totalLength = 0;
+
   request.on("data", (chunk) => {
     array.push(chunk);
+    totalLength += chunk.length;
   });
   request.on("end", () => {
     console.log(array);
-    console.log(Buffer.concat(array));
-    const body = iconv.decode(Buffer.concat(array), "GBK");
+    console.log(Buffer.concat(array, totalLength));
+    const body = iconv.decode(Buffer.concat(array, totalLength), "GBK");
     console.log(body);
     response.end("hi");
   });
