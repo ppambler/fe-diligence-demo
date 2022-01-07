0dda9dbe91140cfdfbf05d34c3e21e7930d0d72e
ppambler <allinlbj@gmail.com>
2022-01-07 23:48:09 +0800

    curl post 请求，得到请求消息体

--- nodejs/02-http/node-static-server/index.ts
+++ nodejs/02-http/node-static-server/index.ts
@@ -1,13 +1,23 @@
 import * as http from "http";
 import { IncomingMessage, ServerResponse } from "http";
-
+const iconv = require("iconv-lite");
 const server = http.createServer();
 
 server.on("request", (request: IncomingMessage, response: ServerResponse) => {
   console.log(request.method);
   console.log(request.url);
   console.log(request.headers);
-  response.end("hi");
+  const array = [];
+  request.on("data", (chunk) => {
+    array.push(chunk);
+  });
+  request.on("end", () => {
+    console.log(array);
+    console.log(Buffer.concat(array));
+    const body = iconv.decode(Buffer.concat(array), "GBK");
+    console.log(body);
+    response.end("hi");
+  });
 });
 
 server.listen(8888);
