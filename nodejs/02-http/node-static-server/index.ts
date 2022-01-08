13acb9812df08b79a482262899fa6f36f37dc104
ppambler <allinlbj@gmail.com>
2022-01-08 20:01:13 +0800

    url.parse 解析请求路径

--- nodejs/02-http/node-static-server/index.ts
+++ nodejs/02-http/node-static-server/index.ts
@@ -1,22 +1,25 @@
 import * as http from "http";
 import { IncomingMessage, ServerResponse } from "http";
 import * as fs from "fs";
-import * as path from "path";
+import * as p from "path";
+import * as url from "url";
 const server = http.createServer();
-const publicDir = path.resolve(__dirname, "public"); // 会得到当前目录所在的绝对路径
+const publicDir = p.resolve(__dirname, "public"); // 会得到当前目录所在的绝对路径
 
 server.on("request", (request: IncomingMessage, response: ServerResponse) => {
-  const { url, method, headers } = request;
-  switch (url) {
+  const { url: path, method, headers } = request;
+  const object = url.parse(path);
+  const { pathname, search } = object;
+  switch (pathname) {
     case "/index.html":
-      fs.readFile(path.resolve(publicDir, "index.html"), (error, data) => {
+      fs.readFile(p.resolve(publicDir, "index.html"), (error, data) => {
         if (error) throw error;
         response.setHeader("Content-type", "text/html;charset=utf-8");
         response.end(data.toString());
       });
       break;
     case "/main.js":
-      fs.readFile(path.resolve(publicDir, "main.js"), (error, data) => {
+      fs.readFile(p.resolve(publicDir, "main.js"), (error, data) => {
         if (error) throw error;
         response.setHeader(
           "Content-type",
@@ -26,12 +29,15 @@ server.on("request", (request: IncomingMessage, response: ServerResponse) => {
       });
       break;
     case "/style.css":
-      fs.readFile(path.resolve(publicDir, "style.css"), (error, data) => {
+      fs.readFile(p.resolve(publicDir, "style.css"), (error, data) => {
         if (error) throw error;
         response.setHeader("Content-type", "text/css;charset=utf-8");
         response.end(data.toString());
       });
       break;
+    default:
+      response.statusCode = 404;
+      response.end();
   }
 });
 
