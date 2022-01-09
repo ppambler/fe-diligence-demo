e55a22b88f8826b2fcd77b5d0e420437dc630c49
ppambler <allinlbj@gmail.com>
2022-01-09 16:46:03 +0800

    处理非 Get 请求（405）& 添加缓存选项（Cache-Control）

--- nodejs/02-http/node-static-server/index.ts
+++ nodejs/02-http/node-static-server/index.ts
@@ -6,11 +6,22 @@ import * as url from "url";
 import { off } from "process";
 const server = http.createServer();
 const publicDir = p.resolve(__dirname, "public"); // 会得到当前目录所在的绝对路径
-
+const cacheAge = 3600 * 24 * 90;
 server.on("request", (request: IncomingMessage, response: ServerResponse) => {
   const { url: path, method, headers } = request;
   console.log(path);
   const { pathname, search } = url.parse(path);
+  if (method !== "GET") {
+    response.statusCode = 405;
+    response.end();
+    return;
+  }
+  // if (method === "POST") {
+  //   response.statusCode = 200;
+  //   response.setHeader("content-type", "text/plain;charset=utf8");
+  //   response.end("这是一个假响应");
+  //   return;
+  // }
   // /index.html -> index.html
   let filename = pathname.substring(1);
   if (filename === "") {
@@ -18,7 +29,7 @@ server.on("request", (request: IncomingMessage, response: ServerResponse) => {
   }
   fs.readFile(p.resolve(publicDir, filename), (error, data) => {
     if (error) {
-      // console.log(error);
+      console.log(error);
       if (error.errno === -4058) {
         response.statusCode = 404;
         fs.readFile(p.resolve(publicDir, "404.html"), (error, data) => {
@@ -34,6 +45,9 @@ server.on("request", (request: IncomingMessage, response: ServerResponse) => {
         response.end("服务器繁忙，请稍后再试");
       }
     } else {
+      // 添加缓存
+      response.setHeader("Cache-Control", `public, max-age=${cacheAge}`);
+      // 返回文件内容
       response.end(data);
     }
   });
