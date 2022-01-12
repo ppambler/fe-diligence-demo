c426bc612c950f5db6a521eb5c093bb559bb7eb9
ppambler <allinlbj@gmail.com>
2022-01-12 18:10:12 +0800

    获取你输入的英文单词

--- nodejs/03-http-request/fanyi/src/cli.ts
+++ nodejs/03-http-request/fanyi/src/cli.ts
@@ -1,8 +1,17 @@
 // import * as commander from "commander";
 // const program = new commander.Command();
 import { Command } from "commander";
+import { translate } from "./main";
 const program = new Command();
-program.version("0.0.1").name("fy").usage("<english>");
+program
+  .version("0.0.1")
+  .name("fy")
+  .usage("<word>")
+  .argument("<word>")
+  .action((word) => {
+    console.log(word);
+    translate(word);
+  });
 
 // 解析参数
 program.parse(process.argv);
